5e06a3ca1d1c68daee9580eac48f8a766734d599
Doug Hubbard <doug@dougis.com>
2025-03-23 16:41:51 -0700

    Cursor removed all product and order data and routes, screens, etc

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,51 +1,18 @@
 import { Row, Col } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
-import Product from "../components/Product";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import Paginate from "../components/Paginate";
-import ProductCarousel from "../components/ProductCarousel";
-
-import { useGetProductsQuery } from "../slices/productsApiSlice";
+import { Link } from "react-router-dom";
 
 const HomeScreen = () => {
-  const { pageNumber, keyword } = useParams();
-  const { data, isLoading, error } = useGetProductsQuery({
-    keyword,
-    pageNumber,
-  });
   return (
     <>
-      {!keyword ? (
-        <ProductCarousel />
-      ) : (
-        <Link to="/" className="btn btn-light mb-4">
-          Home
-        </Link>
-      )}
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">
-          {error?.data?.message || error.error}
-        </Message>
-      ) : (
-        <>
-          <h1>Latest Products</h1>
-          <Row>
-            {data.products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-          <Paginate
-            pages={data.pages}
-            page={data.page}
-            keyword={keyword ? keyword : ""}
-          />
-        </>
-      )}
+      <h1>Welcome to Recipe Manager</h1>
+      <Row>
+        <Col>
+          <p>Manage your recipes and sources efficiently.</p>
+          <Link to="/recipes" className="btn btn-primary">
+            View Recipes
+          </Link>
+        </Col>
+      </Row>
     </>
   );
 };
